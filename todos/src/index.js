508e849bf6f64a8630ace8bff04adde8e389f6e0
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-29 16:56:31 -0500

    idk wtf im doing

--- todos/src/index.js
+++ todos/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import './index.css';
 import App from './Components/App';
 import todosReducer from "./Reducers/reducer.js";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-import * as registerServiceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import "todomvc-app-css/index.css";
 import { createBrowserHistory } from "history";
 import { connectRouter, routerMiddleware } from "connected-react-router";
@@ -14,7 +14,8 @@ import { ConnectedRouter } from "connected-react-router";
 
 const history = createBrowserHistory();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-let store = createStore(connectRouter(history)(todosReducer),
+const store = createStore(
+    connectRouter(history)(todosReducer),
     composeEnhancers(
         applyMiddleware(
             routerMiddleware(history) // for dispatching history actions
@@ -22,20 +23,33 @@ let store = createStore(connectRouter(history)(todosReducer),
     )
 );
 
-const Index = () => (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-        {/* comp that procides access to connect for any subcomp to be able to access redux store */}
-        <Provider store={store}>
-            <ConnectedRouter history={history}>
-                <Route render={(props) =>
-                    <App pathname={props.location.pathname} />
-                }>
-                </Route>
-            </ConnectedRouter>
-        </Provider>
-    </BrowserRouter>
+// const Index = () => (
+//     // <BrowserRouter >
+//         {/* comp that procides access to connect for any subcomp to be able to access redux store */}
+//         <Provider store={store}>
+//             <ConnectedRouter history={history} basename={process.env.PUBLIC_URL}>
+//                 <Route render={(props) =>
+//                     <App pathname={props.location.pathname} />
+//                 }>
+//                 </Route>
+//             </ConnectedRouter>
+//         </Provider>
+//     // </BrowserRouter>
+// );
+
+// ReactDOM.render(<Index />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history} basename={process.env.PUBLIC_URL}>
+            <Route render={(props) =>
+                <App pathname={props.location.pathname} />
+            }>
+            </Route>
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById("root")
 );
 
-ReactDOM.render(<Index />, document.getElementById('root'));
-registerServiceWorker();
+serviceWorker.register();
 // first param has to be component
